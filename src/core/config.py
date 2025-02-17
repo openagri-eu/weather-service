9783772c594e9a4902ee4f864b812676a88fbe40
Yiorgos Marinellis <mari.giorgos@gmail.com>
2025-02-17 16:18:36 +0100

    Rename some env vars and add default values

--- src/core/config.py
+++ src/core/config.py
@@ -12,9 +12,9 @@ WEATHER_SRV_PORT = os.environ.get('WEATHER_SRV_PORT', '8000')
 WEATHER_SRV_HOSTNAME = os.environ.get('WEATHER_SRV_HOSTNAME', 'weathersrv')
 
 # DB
-DATABASE_URI = os.environ.get('DATABASE_URI', 'mongodb://root:root@localhost:27017/')
-DATABASE_NAME = os.environ.get('DATABASE_NAME', 'openagridb')
-OPENWEATHERMAP_API_KEY = os.environ.get('OPENWEATHERMAP_API_KEY', '')
+DATABASE_URI = os.environ.get('WEATHER_SRV_DATABASE_URI', 'mongodb://root:root@localhost:27017/')
+DATABASE_NAME = os.environ.get('WEATHER_SRV_DATABASE_NAME', 'openagridb')
+OPENWEATHERMAP_API_KEY = os.environ.get('WEATHER_SRV_OPENWEATHERMAP_API_KEY', '')
 
 # ALLOWED_HOSTS
 EXTRA_ALLOWED_HOSTS = os.environ.get('EXTRA_ALLOWED_HOSTS', "*").replace(' ', '').split(',')
@@ -25,7 +25,7 @@ WEATHER_SRV_GATEKEEPER_USER = os.environ.get('WEATHER_SRV_GATEKEEPER_USER', '')
 WEATHER_SRV_GATEKEEPER_PASSWORD = os.environ.get('WEATHER_SRV_GATEKEEPER_PASSWORD', '')
 
 # JWT
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES', ''))
-KEY = os.environ.get('JWT_KEY', '')
-ALGORITHM = os.environ.get('ALGORITHM', '')
-CRYPT_CONTEXT_SCHEME = os.environ.get('CRYPT_CONTEXT_SCHEME', '')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES', '240'))
+KEY = os.environ.get('JWT_KEY', 'some-key')
+ALGORITHM = os.environ.get('ALGORITHM', 'HS256')
+CRYPT_CONTEXT_SCHEME = os.environ.get('CRYPT_CONTEXT_SCHEME', 'bcrypt')
